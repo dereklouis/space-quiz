44e6ac3accae06243ea2f2aef88112f8ee005af9
Derek Louis <thedereklouis@gmail.com>
2021-06-09 17:17:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -3,21 +3,52 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="appMaster" className="FCAIC">
+      <div id="titleBox" className="FCAIC">
+        <h1 id="title">SPACE</h1>
+        <h2 id="subTitle">QUIZ</h2>
+      </div>
+      <div id="solarSystemContainer" className="FCAIC">
+        <div id="jupiterOrbit" className="FCAIC">
+          <div id="jupiterShadowBox" className="FCAIC">
+            <div id="jupiterShadow" />
+            <img alt="Jupiter" id="jupiter" src="images/jupiter.png" />
+          </div>
+        </div>
+        <div id="marsOrbit" className="FCAIC">
+          <div id="marsShadowBox" className="FCAIC">
+            <div id="marsShadow" />
+            <img alt="Mars" id="mars" src="images/mars.png" />
+          </div>
+        </div>
+        <div id="earthOrbit" className="FCAIC">
+          <div id="moonOrbit" className="FCAIC">
+            <div id="moonShadowBox" className="FCAIC">
+              <div id="moonShadow" />
+              <img alt="Moon" id="moon" src="images/moon.png" />
+            </div>
+          </div>
+          <div id="earthShadowBox" className="FCAIC">
+            <div id="earthShadow" />
+            <img alt="Earth" id="earth" src="images/earth.png" />
+          </div>
+        </div>
+        <div id="venusOrbit" className="FCAIC">
+          <div id="venusShadowBox" className="FCAIC">
+            <div id="venusShadow" />
+            <img alt="Venus" id="venus" src="images/venus.png" />
+          </div>
+        </div>
+        <div id="mercuryOrbit" className="FCAIC">
+          <div id="venusShadowBox" className="FCAIC">
+            <div id="mercuryShadow" />
+            <img alt="Mercury" id="mercury" src="images/mercury.png" />
+          </div>
+        </div>
+        <div id="sunBox">
+          <img alt="Sun" id="sun" src="images/the-sun.png" />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/questions.js
@@ -0,0 +1,277 @@
+export const questions = {
+  1: [
+    'The stones of Stonehenge are aligned with the ___.',
+    ['Solstice', 'Equinox', 'Full Moon', 'North Pole'],
+    'a',
+  ],
+  2: [
+    'Between the years 185 & 1604, there were at least eight historical accounts of what astronomical event?',
+    ['Lunar Impact', 'Supernova', 'Total Solar Eclipse', 'Transit Of Venus'],
+    'b',
+  ],
+  3: [
+    'In 1543, ___ proposes that the Earth travels around the Sun.',
+    ['Tycho Brahe', 'Andreas Vesalius', 'Leonard Fuchs', 'Nicolaus Copernicus'],
+    'd',
+  ],
+  4: [
+    'Who built the first reflecting telescope in 1668?',
+    ['Johannes Kepler', 'Galileo Galilei', 'James Gregory', 'Issac Newton'],
+    'd',
+  ],
+  5: [
+    'In 1676, Danish astronomer Ole Roemer measured the speed of light using what planet?',
+    ['Mercury', 'Venus', 'Jupiter', 'Saturn'],
+    'c',
+  ],
+  6: [
+    'What German-born British composer/astronomer discovered the planet Uranus in 1781?',
+    ['Edmond Halley', 'Charles Messier', 'Anders Celsius', 'William Herschel'],
+    'd',
+  ],
+  7: [
+    'What was Charles Messier looking for when he created his "Messier Catalogue"?',
+    ['Nebulae', 'Galaxies', 'Comets', 'Globular Clusters'],
+    'c',
+  ],
+  8: [
+    'What in the solar system was discovered in 1845 using mathematical predictions?',
+    ['Asteroid Belt', 'Oort Cloud', 'Neptune', 'Pluto'],
+    'c',
+  ],
+  9: [
+    'What did Albert Einstein win his only Nobel Prize for?',
+    [
+      'The Photoelectric Effect',
+      'Special Relativity',
+      'General Relativity',
+      'Unified Field Theory',
+    ],
+    'a',
+  ],
+  10: [
+    'In 1924, ___ announces that the spiral nebula Andromeda is actually a seperate galaxy, ending the notion that the Milky Way was the only galaxy.',
+    ['Eugene Shoemaker', 'Jan Oort', 'Edwin Hubble', 'David Levy'],
+    'c',
+  ],
+  11: [
+    'Which NASA project preceded both Apollo & Gemini?',
+    ['Redstone', 'Agena', 'Mercury', 'Skylab'],
+    'c',
+  ],
+  12: [
+    'Who was the first American to reach space?',
+    ['Deke Slayton', 'John Glenn', 'Wally Schirra', 'Alan Shepard'],
+    'd',
+  ],
+  13: [
+    'Which Apollo mission brought humans beyond low earth orbit for the first time?',
+    ['Apollo 1', 'Apollo 8', 'Apollo 9', 'Apollo 10'],
+    'b',
+  ],
+  14: [
+    'Who was the flight director during the apollo 11 lunar landing?',
+    ['Ken Mattingly', 'Gene Kranz', 'John Young', 'Glynn Lunney'],
+    'b',
+  ],
+  15: [
+    'Who was the command module pilot of Apollo 11?',
+    ['Tom Hanks', 'Michael Collins', 'Neil Armstrong', 'Buz Aldrin'],
+    'b',
+  ],
+  16: [
+    'Which Apollo mission was struck by lightning shorty after launch?',
+    ['Apollo 12', 'Apollo 13', 'Apollo 14', 'Apollo 15'],
+    'a',
+  ],
+  17: [
+    'What did Neil Armstrong say next after his famous "That\'s one small step for (a) man; one giant leap for mankind" quote?',
+    [
+      'There seems to be no difficulty in moving around.',
+      'Yes, the surface is fine and powdery.',
+      'The descent engine did not leave a crater of any size.',
+      "We're essentially on a very level place here.",
+    ],
+    'b',
+  ],
+  18: [
+    'Which Apollo mission was the first to include a Lunar Rover?',
+    ['Apollo 15', 'Apollo 16', 'Apollo 17', 'Apollo 18'],
+    'a',
+  ],
+  19: [
+    'What was the last Apollo mission?',
+    ['Apollo 16', 'Apollo 17', 'Apollo 18', 'Apollo 19'],
+    'b',
+  ],
+  20: [
+    'In 1984, Bruce McCandless completed the first untethered spacewalk by using the ___.',
+    ['AMU', 'ASMU', 'MMU', 'HHMU'],
+    'c',
+  ],
+  21: [
+    'Which country has NOT had a citizen visit the ISS? ',
+    ['South Africa', 'United Arab Emirates', 'Malaysia', 'China'],
+    'd',
+  ],
+  22: [
+    'How fast does the ISS travel around the Earth?',
+    ['2,288 mph', '7,000 mph', '13,000 mph', '17,500 mph'],
+    'd',
+  ],
+  23: [
+    'The ___ brothers are the only siblings to have both traveled in space.',
+    ['Hadfield', 'Kelly', 'Reisman', 'Cassidy'],
+    'b',
+  ],
+  24: [
+    'What ISS module provides stunning views of the Earth below?',
+    ['Bigelow', 'JEM', 'Cupola', 'MRM'],
+    'c',
+  ],
+  25: [
+    'What country has provided the ISS with two large, external robotic arms?',
+    ['Switzerland', 'Germany', 'Canada', 'Italy'],
+    'c',
+  ],
+  26: [
+    'The ISS has had a continuous human presence since ____.',
+    ['2000', '2001', '2002', '2003'],
+    'a',
+  ],
+  27: [
+    'About how many miles above the earth does the ISS orbit?',
+    ['200', '250', '300', '350'],
+    'a',
+  ],
+  28: [
+    'What happens to urine on the ISS?',
+    [
+      'It is discarded out into space',
+      'It is used as fertilizer in plant experiments',
+      'It is stored and sent back to Earth for testing',
+      'It is recycled into drinking water',
+    ],
+    'd',
+  ],
+  29: [
+    'What year did the Space Shuttle program conclude?',
+    ['2011', '2013', '2015', '2017'],
+    'a',
+  ],
+  30: [
+    'In 2018, European astronaut Alexander Gerst temporarily plugged up a leak in the ISS with ___.',
+    ['Bubble gum', 'Trash', 'A sock', 'His finger'],
+    'd',
+  ],
+  31: [
+    'What planet do the Galilean moons orbit?',
+    ['Venus', 'Mars', 'Jupiter', 'Saturn'],
+    'c',
+  ],
+  32: [
+    'Which Galilean moon is suspected to have deep liquid water oceans?',
+    ['Europa', 'Ganymede', 'Callisto', 'Io'],
+    'a',
+  ],
+  33: [
+    'What planet has a rotational axis of over 90°?',
+    ['Uranus', 'Neptune', 'Venus', 'Saturn'],
+    'a',
+  ],
+  34: [
+    'What spacecraft has traveled the farthest from Earth?',
+    ['Voyager 1', 'Voyager 2', 'New Horizons', 'Cassini'],
+    'a',
+  ],
+  35: [
+    'What planet has a day length nearly identical to Earth?',
+    ['Neptune', 'Mars', 'Mercury', 'Venus'],
+    'b',
+  ],
+  36: [
+    'The "Pale Blue Dot" photograph was taken after Voyager 1 passed what planet?',
+    ['Jupiter', 'Saturn', 'Uranus', 'Neptune'],
+    'b',
+  ],
+  37: [
+    'The word planet comes from the Greek word "planetes", meaning ___.',
+    ['Pearl', 'Season', 'Priest', 'Wanderer'],
+    'd',
+  ],
+  38: [
+    'What planet hosts the small, oblong moons Phobos & Deimos?',
+    ['Mercury', 'Venus', 'Mars', 'Neptune'],
+    'c',
+  ],
+  39: [
+    'What was the first rover to rove on Mars?',
+    ['Spirit', 'Curiosity', 'Opportunity', 'Sojourner'],
+    'd',
+  ],
+  40: [
+    'What type of star is our sun?',
+    ['Yellow Dwarf', 'Red Giant', 'White Dwarf', 'Brown Dwarf'],
+    'a',
+  ],
+  41: [
+    'What NASA observatory is built into a Boeing-747?',
+    ['CERN', 'James Webb', 'Hubble', 'SOFIA'],
+    'd',
+  ],
+  42: [
+    'What US observatory detected gravitational waves in 2015?',
+    ['LIGO', 'SOPHIA', 'CERN', 'SNOLAB'],
+    'a',
+  ],
+  43: [
+    'What twin telescope array sits near the summit of Mauna Kea, Hawaii?',
+    [
+      'Large Binocular Telescope',
+      'Keck',
+      'Gran Telescopio Canarias',
+      'Hobby-Eberly Telescope',
+    ],
+    'b',
+  ],
+  44: [
+    'What will the James Webb space telescope orbit?',
+    ['The Moon', 'Pluto', 'Earth-Moon L2', 'Sun-Earth L2'],
+    'd',
+  ],
+  45: [
+    "The ___ telescope, currently under construction in Chile, will become the world's largest optical/near-infrared telescope.",
+    ['Supermassive', 'Extremely Large', 'Colossal', 'Herculean'],
+    'b',
+  ],
+  46: [
+    'What project plans to use a ground based laser to accelerate small probes to Alpha Centauri and beyond?',
+    ['Starlink', 'Project Kuiper', 'Hongyun Project', 'Breakthrough Starshot'],
+    'd',
+  ],
+  47: [
+    'What SpaceX launch vehicle currently under development, plans to bring humans to the Moon, Mars and beyond?',
+    ['Falcon', 'Dragon', 'Starship', 'Falcon Heavy'],
+    'c',
+  ],
+  48: [
+    'What aerospace manufacturer was founded by Jeff Bezos in 2000?',
+    [
+      'Rocket Lab',
+      'Blue Origin',
+      'United Launch Alliance',
+      'Sierra Nevada Corporation',
+    ],
+    'b',
+  ],
+  49: [
+    'What NASA mission currently in progress, aims to collect and return a sample from near-Earth asteroid Bennu?',
+    ['ASTER', 'InSight', 'NEOWISE', 'OSIRIS-REx'],
+    'd',
+  ],
+  50: [
+    'What NASA program aims to return humans to the moon by 2024?',
+    ['Artemis', 'Athena', 'Hermes', 'Ersa'],
+    'a',
+  ],
+};
